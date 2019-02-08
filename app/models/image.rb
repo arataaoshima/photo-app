43f1341d9c669428372eb8a5e8f117b630ad23e4
ARATA <newbluelight1031@gmail.com>
2019-02-08 05:24:04 +0000

    install imageminick

--- app/models/image.rb
+++ app/models/image.rb
@@ -5,8 +5,10 @@ class Image < ActiveRecord::Base
   
   private
    def picture_size
+ 
      if picture.size > 5.megabytes
        errors.add(:picture, "should be less than 5MB")
+       
      end
    end
 end
